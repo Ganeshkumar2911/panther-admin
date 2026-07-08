91a9c7656cb321b12f526800a7f4268c5bafa5a2
Vivek-Jayaswal <vivekkumarjaiswal2000@gmail.com>
2026-07-08 16:52:57 +0530

    time zone issue fix in payment request

--- src/utils/timeFormatter.js
+++ src/utils/timeFormatter.js
@@ -1,10 +1,15 @@
+import moment from "moment-timezone";
+
 export const formatDate = (date) => {
-  return new Intl.DateTimeFormat(undefined, {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    timeZoneName: "short",
-  }).format(new Date(date));
+  // return new Intl.DateTimeFormat("en-US", {
+  //   year: "numeric",
+  //   month: "short",
+  //   day: "numeric",
+  //   hour: "2-digit",
+  //   minute: "2-digit",
+  //   // timeZoneName: "short",
+  //   timeZoneName: "shortOffset",
+  // }).format(new Date(date));
+  const tz = moment.tz.guess();
+  return moment.utc(date).tz(tz).format("MMM D, YYYY, hh:mm A z");
 };
