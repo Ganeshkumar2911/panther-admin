52bf1a4a2b8b3ec2dd8af4c0051702e660fb5903
Vivek-Jayaswal <vivekkumarjaiswal2000@gmail.com>
2026-07-14 12:23:14 +0530

    update : base url change

--- src/api/request.js
+++ src/api/request.js
@@ -6,8 +6,8 @@ import router from "../router";
 
 // const BASE_URL = "https://w2llv2cm-2504.inc1.devtunnels.ms/admin/";
 // const BASE_URL = "https://f7v2d03l-2504.inc1.devtunnels.ms/admin/";
-const BASE_URL = "https://848ncvt5-2504.euw.devtunnels.ms/admin/";
-// const BASE_URL = "https://1pz4zm0b-2504.euw.devtunnels.ms/admin/";
+// const BASE_URL = "https://848ncvt5-2504.euw.devtunnels.ms/admin/";
+const BASE_URL = "https://1pz4zm0b-2504.euw.devtunnels.ms/admin/";
 const DEFAULT_TIMEOUT = 2 * 60 * 1000;
 const MAX_RETRY_ATTEMPTS = 2;
 const RETRYABLE_STATUS_CODES = [502, 503, 504];
